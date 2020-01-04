0c8cac74e3da775096bfaabf5b0784bbc92b0622
Dano Sato <uchang01@gmail.com>
2020-01-05 05:41:07 +0900

    Make DOmParser

--- DomParser.js
+++ DomParser.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var BDom = require('beautiful-dom');
 var path = '/Users/satodano/ImgReader.txt';
 var m, urls = [];
 var str2 = fs.readFileSync(path, 'utf8', function (err, data) {
@@ -6,11 +7,30 @@ var str2 = fs.readFileSync(path, 'utf8', function (err, data) {
         return console.error(err);
     }
 });
-var str = '<img src="http://site.org/one.jpg />\n <img src="http://site.org/two.jpg />', rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
-while (m = rex.exec(str2)) {
-    urls.push(m[1]);
+//Get whole string from the Img processor file.
+//Process the html code to get img sources.
+//Regular Expression method.
+/*
+var str = '<img src="http://site.org/one.jpg />\n <img src="http://site.org/two.jpg />',
+    rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g,
+    rex2 = ;
+while ( m = rex.exec(str2))
+{
+  urls.push( m[1] );
+}*/
+//Didn't Work.
+var dom = new BDom(str2);
+var ImgNode = dom.querySelectorAll('img');
+var ProcessedStr = '';
+for (var _i = 0, ImgNode_1 = ImgNode; _i < ImgNode_1.length; _i++) {
+    var val = ImgNode_1[_i];
+    console.log(val.outerHTML);
+    ProcessedStr += val.outerHTML;
+    ProcessedStr += "\n";
 }
-fs.writeFile(path, urls, function (err) {
+ProcessedStr += "^M";
+//Write processed code to the img processor file.
+fs.writeFile(path, ProcessedStr, function (err) {
     if (err) {
         return console.error(err);
     }
--- DomParser.ts
+++ DomParser.ts
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const BDom = require('beautiful-dom');
 
 
 let path='/Users/satodano/ImgReader.txt';
@@ -19,17 +19,31 @@ let str2 = fs.readFileSync(path,'utf8',function (err, data)
 
 
 //Process the html code to get img sources.
+//Regular Expression method.
+/*
 var str = '<img src="http://site.org/one.jpg />\n <img src="http://site.org/two.jpg />',
     rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g,
     rex2 = ;
 while ( m = rex.exec(str2)) 
 {
   urls.push( m[1] );
-}
+}*/
+//Didn't Work.
+
+let dom=new BDom(str2);
+let ImgNode = dom.querySelectorAll('img')
+let ProcessedStr = '';
+for(var val of ImgNode)
+  {
+    console.log(val.outerHTML)
+    ProcessedStr+=val.outerHTML;
+    ProcessedStr+="\n";
+  }
+ProcessedStr+="^M";
 
 
 //Write processed code to the img processor file.
-fs.writeFile(path,urls, function(err) {
+fs.writeFile(path,ProcessedStr, function(err) {
   if(err) {
     return console.error(err);
   } console.log("File created!");
