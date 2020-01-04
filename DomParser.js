04e83ee278fc7207314d631414a20b7372e83485
Dano Sato <uchang01@gmail.com>
2020-01-05 04:42:52 +0900

    commit

--- DomParser.js
+++ DomParser.js
@@ -1,15 +1,18 @@
-var ImgReader = /** @class */ (function () {
-    function ImgReader() {
+var fs = require('fs');
+var path = '/Users/satodano/ImgReader.txt';
+var m, urls = [];
+var str2 = fs.readFileSync(path, 'utf8', function (err, data) {
+    if (err) {
+        return console.error(err);
     }
-    ImgReader.prototype.Exam = function (code) {
-        console.log(code.match(/<img[^>]+src="http([^">]+)/g));
-    };
-    return ImgReader;
-}());
-var m, urls = [], str = '<img src="http://site.org/one.jpg />\n <img src="http://site.org/two.jpg />', rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
-while (m = rex.exec(str)) {
+});
+var str = '<img src="http://site.org/one.jpg />\n <img src="http://site.org/two.jpg />', rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
+while (m = rex.exec(str2)) {
     urls.push(m[1]);
 }
-console.log(urls);
-var t = new ImgReader();
-t.Exam("<img src=http://static2.ccn.com/ccs/2013/02/img_example.jpg />");
+fs.writeFile(path, urls, function (err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("File created!");
+});
--- DomParser.ts
+++ DomParser.ts
@@ -1,24 +1,38 @@
-class ImgReader
-{ 
-  Exam(code: string)
-  {
-    
-    console.log(code.match(/<img[^>]+src="http([^">]+)/g)); 
-  }
-}
+const fs = require('fs');
 
+
+
+let path='/Users/satodano/ImgReader.txt';
 var m,
-    urls = [], 
-    str = '<img src="http://site.org/one.jpg />\n <img src="http://site.org/two.jpg />',
-    rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
+    urls = [];
+let str2 = fs.readFileSync(path,'utf8',function (err, data)
+                         {
+                           if (err)
+                             {
+                               return console.error(err);
+                             }
+                         }
+                  
+                        );
 
-while ( m = rex.exec(str)) 
+//Get whole string from the Img processor file.
+
+
+//Process the html code to get img sources.
+var str = '<img src="http://site.org/one.jpg />\n <img src="http://site.org/two.jpg />',
+    rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g,
+    rex2 = ;
+while ( m = rex.exec(str2)) 
 {
   urls.push( m[1] );
 }
 
-console.log(urls)
-let t = new ImgReader();
 
-t.Exam(`<img src=http://static2.ccn.com/ccs/2013/02/img_example.jpg />`);
+//Write processed code to the img processor file.
+fs.writeFile(path,urls, function(err) {
+  if(err) {
+    return console.error(err);
+  } console.log("File created!");
+});
+
 
